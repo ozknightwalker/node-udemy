d0a3ded41648299b7f9124653dcc070667102e8e
Jc Din <ozknightwalker62@gmail.com>
2019-11-08 14:30:30 +0800

    add json and home routes that return json and renders html

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express);
 
 app.get('/', (request, response, next) => {
     response.send('Hello Express!!');
+});
+
+app.get('/json', (request, response, next) => {
+    const data = {
+        'greeting': 'Hello!!'
+    };
+    response.json(data);
+});
+
+app.get('/home', (request, response, next) => {
+    response.render('home', null)
 });
 
 app.listen(3000);
